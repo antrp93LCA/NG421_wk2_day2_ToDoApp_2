40f4844975dcc9cdec73fc9db927b3cb7603bdf7
Anthony Perez <antrp93@gmail.com>
2020-04-10 21:31:25 -0500

    imported NgbModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { CreateTodoComponent } from './create-todo/create-todo.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./todo/todo.component";
+import { CreateTodoComponent } from "./create-todo/create-todo.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     CreateTodoComponent,
-    TodoListComponent
+    TodoListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
